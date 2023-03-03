9d1d78fe451f33ff6da8401fbe154b0d949355e0
XhammerTeam <124336693+XhammerTeam@users.noreply.github.com>
2023-03-03 09:08:48 +0300

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,32 +1,4 @@
-cd ..
-pkg install figlet
-apt install figlet
-mkdir XhammerTeam
-cd XhammerTeam
-mkdir Tools
-mkdir Tests
-mkdir Storage
-mkdir Pictures
-mkdir Downloads
-mkdir Documents
-mkdir Apps
-cd ..
 git clone https://github.com/XhammerTeam/LootEx
 cd LootEx
 chmod +x LootEx.sh
 bash LootEx.sh
-cd XhammerTeam
-cd Tools
-git clone https://github.com/XhammerTeam/FUCKDT
-cd FUCKDT
-ls
-chmod +x FuckDT.sh
-cd ..
-clear
-printf "\e[1;92m"
-figlet Xhammer
-printf "\n\n"
-printf "Welcome To Xhammer Setup!"
-printf "\n\n"
-defps1="PS1='\\\[\\\e[39m\\\]\\\[\\\e[32m\\\]~ \\\[\\\e[0m\\\]\\\[\\\e[31m\\\][[ \\\[\\\e[0m\\\]\\\[\\\e[32m\\\]Xhammer Code\\\[\\\e[0m\\\]\\\[\\\e[31m\\\] ]]\\\[\\\e[0m\\\]\\\[\\\e[31m\\\] ~ \\\[\\\e[0m\\\]\\\[\\\e[32m\\\] \\\[\\\e[0m\\\]\\\[\\\e[0m\\\]\\\[\\\e[32m\\\]'"
-sed -i "s/.*PS1=.*/$defps1/" /data/data/com.termux/files/usr/etc/bash.bashrc
